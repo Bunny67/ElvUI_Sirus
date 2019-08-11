8345be02343b5227af956e45b76c4155beff9cd7
bunny67 <zolov6767@gmail.com>
2019-08-11 19:56:43 +0300

    Update Skins.lua

--- ElvUI_Sirus/Modules/Skins/Skins.lua
+++ ElvUI_Sirus/Modules/Skins/Skins.lua
@@ -7,13 +7,20 @@ local tremove = table.remove
 --WoW API / Variables
 
 function S:RemoveCallback(eventName)
+	if not eventName or type(eventName) ~= "string" then
+		E:Print("Invalid argument #1 to S:RemoveCallback (string expected)")
+		return
+	elseif not self.nonAddonCallbacks[eventName] then
+		E:Print("Invalid 'eventName' #1 to S:RemoveCallback")
+		return
+	end
+
 	self.nonAddonCallbacks[eventName] = nil
 	for index, event in ipairs(self.nonAddonCallbacks.CallPriority) do
 		if event == eventName then
 			tremove(self.nonAddonCallbacks.CallPriority, index)
 		end
 	end
-
 	E.UnregisterCallback(S, eventName)
 end
 
