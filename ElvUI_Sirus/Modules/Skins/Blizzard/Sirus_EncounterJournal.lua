9ada23b8cbf2a0b5389a4fefbb7c14fbebb32d44
bunny67 <zolov6767@gmail.com>
2019-10-05 13:06:03 +0300

    Update Sirus_EncounterJournal.lua

--- ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_EncounterJournal.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_EncounterJournal.lua
@@ -92,15 +92,6 @@ local function LoadSkin()
 	SkinEncounterTab(EncounterJournal.encounter.info.modelTab)
 	EncounterJournal.encounter.info.modelTab:Point("TOP", EncounterJournal.encounter.info.bossTab, "BOTTOM", 0, -1)
 
-	hooksecurefunc("EncounterJournal_DisplayInstance", function(instanceID)
-		local self = EncounterJournal.encounter
-		if EncounterJournal_SearchForOverview(instanceID) then
-			self.info.modelTab:SetPoint("TOP", self.info.bossTab, "BOTTOM", 0, -1)
-		else
-			self.info.modelTab:SetPoint("TOP", self.info.lootTab, "BOTTOM", 0, -1)
-		end
-	end)
-
 	S:HandleScrollBar(EncounterJournal.encounter.info.bossesScroll.ScrollBar)
 	EncounterJournal.encounter.info.bossesScroll.child.description:SetTextColor(1, 1, 1)
 
@@ -117,9 +108,142 @@ local function LoadSkin()
 
 	S:HandleScrollBar(EncounterJournal.encounter.info.lootScroll.scrollBar)
 
+	for i = 1, #EncounterJournal.encounter.info.lootScroll.buttons do
+		local item = EncounterJournal.encounter.info.lootScroll.buttons[i]
+		item.icon:SetDrawLayer("BORDER")
+		S:HandleIcon(item.icon)
+
+		item.bossTexture:SetAlpha(0)
+		item.bosslessTexture:SetAlpha(0)
+
+		item.armorType:SetTextColor(1, 1, 1)
+		item.slot:SetTextColor(1, 1, 1)
+		item.boss:SetTextColor(1, 1, 1)
+
+		item.IconBorder:SetAlpha(0)
+		hooksecurefunc(item.IconBorder, "SetVertexColor", function(_, r, g, b) item.backdrop:SetBackdropBorderColor(r, g, b) end)
+		hooksecurefunc(item.IconBorder, "Hide", function() item.backdrop:SetBackdropBorderColor(unpack(E.media.bordercolor)) end)
+	end
+
 	EncounterJournal.LootJournal:StripTextures()
 	S:HandleButton(EncounterJournal.LootJournal.ItemSetsScrollFrame.ClassButton, true)
 	S:HandleScrollBar(EncounterJournalLootJournalScrollFrameScrollBar)
+
+	for i = 1, #EncounterJournal.LootJournal.ItemSetsScrollFrame.buttons do
+		local set = EncounterJournal.LootJournal.ItemSetsScrollFrame.buttons[i]
+		set.Background:Hide()
+		set.ItemLevel:SetTextColor(1, 1, 1)
+
+		for j = 1, 8 do
+			local button = set["ItemButton"..j]
+			button:SetTemplate()
+			button.Icon:SetDrawLayer("BORDER")
+			button.Icon:SetInside()
+			button.Icon:SetTexCoord(unpack(E.TexCoords))
+
+			button.Border:SetAlpha(0)
+		end
+
+		hooksecurefunc(set.SetName, "SetTextColor", function(_, r, g, b)
+			for j = 1, 8 do
+				local button = set["ItemButton"..j]
+				button:SetBackdropBorderColor(r, g, b)
+			end
+		end)
+	end
+
+	hooksecurefunc("EncounterJournal_ListInstances", function()
+		local scrollFrame = EncounterJournal.instanceSelect.scroll.child
+
+		local index = 1
+		local instanceButton = scrollFrame["instance"..index]
+
+		while instanceButton do
+			if not instanceButton.isSkinned then
+				S:HandleButton(instanceButton)
+
+				instanceButton.bgImage:SetInside()
+				instanceButton.bgImage:SetTexCoord(.08, .6, .08, .6)
+				instanceButton.bgImage:SetDrawLayer("BORDER")
+
+				instanceButton.name:SetTextColor(1, 1, 1)
+
+				instanceButton.isSkinned = true
+			end
+
+			index = index + 1
+			instanceButton = scrollFrame["instance"..index]
+		end
+	end)
+
+	hooksecurefunc("EncounterJournal_DisplayInstance", function(instanceID)
+		local self = EncounterJournal.encounter
+		if EncounterJournal_SearchForOverview(instanceID) then
+			self.info.modelTab:SetPoint("TOP", self.info.bossTab, "BOTTOM", 0, -1)
+		else
+			self.info.modelTab:SetPoint("TOP", self.info.lootTab, "BOTTOM", 0, -1)
+		end
+
+		local bossIndex = 1
+		local bossButton = _G["EncounterJournalBossButton"..bossIndex]
+		while bossButton do
+			if not bossButton.isSkinned then
+				S:HandleButton(bossButton)
+
+				bossButton.creature:ClearAllPoints()
+				bossButton.creature:Point("TOPLEFT", 1, -4)
+
+				bossButton.text:SetTextColor(1, 1, 1)
+
+				bossButton.isSkinned = true
+			end
+
+			bossIndex = bossIndex + 1
+			bossButton = _G["EncounterJournalBossButton"..bossIndex]
+		end
+	end)
+
+	hooksecurefunc("EncounterJournal_ToggleHeaders", function(self, _, index)
+		local headerCount = 1
+		local header = _G["EncounterJournalInfoHeader"..headerCount]
+		while header do
+			if not header.isSkinned then
+				header.descriptionBG:SetAlpha(0)
+				header.descriptionBGBottom:SetAlpha(0)
+
+				for i = 4, 18 do
+					select(i, header.button:GetRegions()):SetTexture()
+				end
+
+				S:HandleButton(header.button)
+
+				header.button.title:SetTextColor(unpack(E.media.rgbvaluecolor))
+				header.button.title.SetTextColor = E.noop
+				header.button.expandedIcon:SetTextColor(1, 1, 1)
+				header.button.expandedIcon.SetTextColor = E.noop
+
+				header.description:SetTextColor(1, 1, 1)
+				header.description.SetTextColor = E.noop
+
+				header.isSkinned = true
+			end
+
+			headerCount = headerCount + 1
+			header = _G["EncounterJournalInfoHeader"..headerCount]
+		end
+	end)
+
+	hooksecurefunc("EncounterJournal_SetBullets", function(object)
+		local parent = object:GetParent()
+		if parent.Bullets then
+			for _, bullet in pairs(parent.Bullets) do
+				if not bullet.isSkinned then
+					bullet.Text:SetTextColor(1, 1, 1)
+					bullet.isSkinned = true
+				end
+			end
+		end
+	end)
 end
 
 S:AddCallback("Sirus_EncounterJournal", LoadSkin)
