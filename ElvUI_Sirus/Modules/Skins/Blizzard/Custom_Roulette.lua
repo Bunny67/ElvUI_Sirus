2f0ac7a7540e5f955fea51e7b9da5ee1beabc036
Bunny67 <zolov6767@gmail.com>
2020-01-19 17:50:13 +0300

    Update Custom_Roulette.lua

--- ElvUI_Sirus/Modules/Skins/Blizzard/Custom_Roulette.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/Custom_Roulette.lua
@@ -13,17 +13,92 @@ local function LoadSkin()
 
 	Custom_RouletteFrame:StripTextures(true)
 	Custom_RouletteFrame:SetTemplate("Transparent")
+	Custom_RouletteFrame:SetSize(804, 600)
 
 	S:HandleCloseButton(Custom_RouletteFrame.closeButton)
 
 	Custom_RouletteFrame.HeaderFrame:StripTextures()
-	Custom_RouletteFrame.HeaderFrame:SetPoint("TOP", 0, 0)
-	Custom_RouletteFrame.HeaderFrame.TitleText:FontTemplate(nil, 16, "NONE")
+	Custom_RouletteFrame.HeaderFrame:SetPoint("TOP", 0, 6)
+	Custom_RouletteFrame.HeaderFrame.TitleText:FontTemplate(nil, 18, "NONE")
+	
+	Custom_RouletteFrame.ToggleCurrencyFrame.Background:SetTexture(E.Media.Textures.Highlight)
+	Custom_RouletteFrame.ToggleCurrencyFrame.Background:SetTexCoord(1, 0, 1, 0)
+	Custom_RouletteFrame.ToggleCurrencyFrame.Background:SetAlpha(0.3)
+
+	local function OnEnter(button)
+		button.Text:SetTextColor(1, 1, 1)
+	end
+
+	local function OnLeave(button)
+		if not button.active then
+			button.Text:SetTextColor(unpack(E.media.rgbvaluecolor))
+		end
+	end
+
+	local function OnClick(self)
+		for _, button in pairs(self:GetParent().currencyButtons) do
+			if button.active then
+				button.Text:SetTextColor(1, 1, 1)
+			else
+				button.Text:SetTextColor(unpack(E.media.rgbvaluecolor))
+			end
+		end
+	end
+
+	local function SkinRouletteCurrencyButto(button)
+		button.Text:FontTemplate(nil, 14, "NONE")
+
+		if button:GetID() == 1 then
+			button.Text:SetTextColor(1, 1, 1)
+			button.active = true
+		else
+			button.Text:SetTextColor(unpack(E.media.rgbvaluecolor))
+		end
+
+		button:HookScript("OnEnter", OnEnter)
+		button:HookScript("OnLeave", OnLeave)
+		button:HookScript("OnClick", OnClick)
+
+		button:SetFrameLevel(Custom_RouletteFrame.ToggleCurrencyFrame:GetFrameLevel() + 3)
+	end
+
+	SkinRouletteCurrencyButto(Custom_RouletteFrame.ToggleCurrencyFrame.CurrencyBonus)
+	SkinRouletteCurrencyButto(Custom_RouletteFrame.ToggleCurrencyFrame.CurrencyLuckCoins)
+	
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetSize(200, 48)
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:ClearAllPoints()
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetPoint("CENTER")
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetTexture(E.Media.Textures.Highlight)
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetVertexColor(unpack(E.media.rgbvaluecolor))
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetTexCoord(1, 0, 1, 0)
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector.Selector:SetAlpha(0.5)
+	Custom_RouletteFrame.ToggleCurrencyFrame.CurrencySelector:SetFrameLevel(Custom_RouletteFrame.ToggleCurrencyFrame:GetFrameLevel() + 1)
+
+	Custom_RouletteFrame.OverlayFrame:SetPoint("CENTER", -2, 140)
+	Custom_RouletteFrame.OverlayFrame.Background:SetAlpha(0)
+	Custom_RouletteFrame.OverlayFrame.ArtOverlay:StripTextures()
+
+	local lineTexture = Custom_RouletteFrame.OverlayFrame.ArtOverlay:CreateTexture()
+	lineTexture:Size(3, 122)
+	lineTexture:SetPoint("CENTER")
+	lineTexture:SetTexture(0.8, 0, 0)
+
+	Custom_RouletteFrame.SpinButton:StripTextures(true)
+	Custom_RouletteFrame.SpinButton:SetHeight(48)
+	Custom_RouletteFrame.SpinButton:SetPoint("CENTER", 0, 44)
+	S:HandleButton(Custom_RouletteFrame.SpinButton)
 
 	for i = 1, #Custom_RouletteFrame.itemButtons do
 		local button = Custom_RouletteFrame.itemButtons[i]
 		button.OverlayFrame.ChildFrame.ItemName:FontTemplate(nil, 12, "NONE")
 	end
+
+	Custom_RouletteFrame.RewardItemsFrame:SetHeight(304)
+	Custom_RouletteFrame.RewardItemsFrame.TitleFrame.Background:SetTexture(E.Media.Textures.Highlight)
+	Custom_RouletteFrame.RewardItemsFrame.TitleFrame.Background:SetTexCoord(1, 0, 1, 0)
+	Custom_RouletteFrame.RewardItemsFrame.TitleFrame.Background:SetAlpha(0.3)
+	Custom_RouletteFrame.RewardItemsFrame.TitleFrame.Text:FontTemplate(nil, 16, "NONE")
+	Custom_RouletteFrame.RewardItemsFrame.TitleFrame.Text:SetTextColor(1, 1, 1)
 
 	for i = 1, #Custom_RouletteFrame.rewardButtons do
 		local button = Custom_RouletteFrame.rewardButtons[i]
