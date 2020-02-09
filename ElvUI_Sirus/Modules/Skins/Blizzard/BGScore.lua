b59cf25944a19eebd7b24b454b4b7f251f39f1b2
Bunny67 <zolov6767@gmail.com>
2020-02-09 21:27:21 +0300

    Update BGScore.lua

--- ElvUI_Sirus/Modules/Skins/Blizzard/BGScore.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/BGScore.lua
@@ -11,9 +11,10 @@ local function LoadSkin()
 	WorldStateScoreFrame:SetTemplate("Transparent")
 
 	BattlegroundBalanceProgressBar:CreateBackdrop()
-	BattlegroundBalanceProgressBar.backdrop:SetOutside(BattlegroundBalanceProgressBar.Horde, nil, nil, BattlegroundBalanceProgressBar.Alliance)
+	BattlegroundBalanceProgressBar.backdrop:SetOutside(BattlegroundBalanceProgressBar.Alliance, nil, nil, BattlegroundBalanceProgressBar.Horde)
 	BattlegroundBalanceProgressBar.Border:SetAlpha(0)
 
+	WorldStateScoreFrame.Container.FactionGlow:SetPoint("TOPLEFT", 1, 12)
 	WorldStateScoreFrame.Container.Separator:SetAlpha(0)
 	WorldStateScoreFrame.Container.Bg:SetAlpha(0)
 	WorldStateScoreFrame.Container.TopRightCorner:SetAlpha(0)
@@ -36,6 +37,17 @@ local function LoadSkin()
 	end
 
 	S:HandleButton(WorldStateScoreFrameLeaveButton)
+
+	WorldStateScoreFrame.EfficiencyFrame.EfficiencyBar:CreateBackdrop()
+	WorldStateScoreFrame.EfficiencyFrame.EfficiencyBar:SetStatusBarTexture(E.media.normTex)
+	E:RegisterStatusBar(WorldStateScoreFrame.EfficiencyFrame.EfficiencyBar)
+	WorldStateScoreFrame.EfficiencyFrame.EfficiencyBar.Border:SetAlpha(0)
+
+	WorldStateScoreFrame.EfficiencyFrame.EfficiencyBar:HookScript("OnValueChanged", function(self, value)
+		local r, g, b = E:ColorGradient(value / 1, .65,0,0, .65,.65,0, 0,.65,0)
+		self:SetStatusBarColor(r, g, b)
+		self.Background:SetTexture(r * 0.25, g * 0.25, b * 0.25)
+	end)
 end
 
 S:RemoveCallback("Skin_WorldStateScore")
