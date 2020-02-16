fe4f09050bcfd470d809d8686b19890ef304c237
Bunny67 <zolov6767@gmail.com>
2020-02-16 11:37:35 +0300

    Revert "Update Data.lua"
    This reverts commit 86a3d577d0b0840dcc58ad2c9a7c2c677938f6ab.

--- ElvUI_Sirus/Plugins/VisualProcs/Data.lua
+++ ElvUI_Sirus/Plugins/VisualProcs/Data.lua
@@ -8,16 +8,9 @@ tinsert(LBP_Data.ButtonProcs.PALADIN[1], 307927)
 
 tinsert(SPELLOVERLAY_STORAGE, {nil, {307927}, {24239, 24274, 24275, 27180, 48805, 48806}})
 
-
---[[
-for i = 307890, 309000 do
-	local name = GetSpellInfo(i)
-	if name then
-		DEFAULT_CHAT_FRAME:AddMessage("\124cffffd000\124Hspell:"..i.."\124h["..name.."]\124h\124r")
-	end
-end
-
 -- Test
-
+--[[
 tinsert(LBP_Data.ButtonProcs.PRIEST[1], 48168)
+LBP_Data.OverlayProcs.PRIEST[48168] = 3
+LBP_Data.OverlayTextures.PRIEST[3] = {10}
 ]]
