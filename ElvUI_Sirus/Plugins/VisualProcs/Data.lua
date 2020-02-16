21446b2590e7dbc18f348c6ddf67a2682aefd05d
Bunny67 <zolov6767@gmail.com>
2020-02-16 10:29:27 +0300

    visual procs

---
+++ ElvUI_Sirus/Plugins/VisualProcs/Data.lua
@@ -0,0 +1,21 @@
+if not LBP_Data then return end
+
+local tinsert = table.insert
+
+-- PALADIN
+-- 2 T5 
+tinsert(LBP_Data.ButtonProcs.PALADIN[1], 307923)
+LBP_Data.OverlayProcs.PALADIN[307923] = 4
+LBP_Data.OverlayTextures.PALADIN[4] = {3}
+
+tinsert(SPELLOVERLAY_STORAGE, {nil, {307923, 3}, {24239, 24274, 24275, 27180, 48805, 48806}})
+
+
+
+
+-- Test
+--[[
+tinsert(LBP_Data.ButtonProcs.PRIEST[1], 48168)
+LBP_Data.OverlayProcs.PRIEST[48168] = 3
+LBP_Data.OverlayTextures.PRIEST[3] = {10}
+]]
