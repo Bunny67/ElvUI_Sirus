44355c6dcbd46219e5f6d6fd3a7629498e155353
Bunny67 <zolov6767@gmail.com>
2020-02-16 10:56:17 +0300

    Update Data.lua

--- ElvUI_Sirus/Plugins/VisualProcs/Data.lua
+++ ElvUI_Sirus/Plugins/VisualProcs/Data.lua
@@ -5,7 +5,6 @@ local tinsert = table.insert
 -- PALADIN
 -- 2 T5
 tinsert(LBP_Data.ButtonProcs.PALADIN[1], 307927)
-LBP_Data.OverlayProcs.PALADIN[307927] = 4
 
 tinsert(SPELLOVERLAY_STORAGE, {nil, {307927}, {24239, 24274, 24275, 27180, 48805, 48806}})
 
