a2e7daf2f98d23fcf367c7ed39b08eb5b3ee881a
Bunny67 <zolov6767@gmail.com>
2020-02-16 11:11:46 +0300

    Update Sirus_ItemSocketing.lua

--- ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_ItemSocketing.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_ItemSocketing.lua
@@ -8,48 +8,85 @@ local GetNumSockets = GetNumSockets
 local GetSocketTypes = GetSocketTypes
 
 local function LoadSkin()
-ItemSocketingFrame:StripTextures()
-	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.socket ~= true then return; end
+	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.socket then return end
 
+	ITEM_SOCKETING_DESCRIPTION_MIN_WIDTH = 278
+
+	ItemSocketingFrame:StripTextures()
 	ItemSocketingFrame:CreateBackdrop("Transparent")
 	ItemSocketingFrame.backdrop:Point("TOPLEFT", 11, -12)
-	ItemSocketingFrame.backdrop:Point("BOTTOMRIGHT", -4, 27)
+	ItemSocketingFrame.backdrop:Point("BOTTOMRIGHT", -2, 31)
+
+	S:SetUIPanelWindowInfo(ItemSocketingFrame, "width")
+	S:SetBackdropHitRect(ItemSocketingFrame)
 
 	ItemSocketingFramePortrait:Kill()
 
-	S:HandleCloseButton(ItemSocketingCloseButton)
+	S:HandleCloseButton(ItemSocketingCloseButton, ItemSocketingFrame.backdrop)
 
+	ItemSocketingScrollFrame:Height(269)
+	ItemSocketingScrollFrame:Point("TOPLEFT", 20, -77)
 	ItemSocketingScrollFrame:StripTextures()
 	ItemSocketingScrollFrame:CreateBackdrop("Transparent")
+	ItemSocketingScrollFrame.backdrop:Point("BOTTOMRIGHT", 3, -2)
 
 	S:HandleScrollBar(ItemSocketingScrollFrameScrollBar, 2)
 
+	ItemSocketingScrollFrameScrollBar:Point("TOPLEFT", ItemSocketingScrollFrame, "TOPRIGHT", 7, -18)
+	ItemSocketingScrollFrameScrollBar:Point("BOTTOMLEFT", ItemSocketingScrollFrame, "BOTTOMRIGHT", 7, 19)
+
+	S:HandleButton(ItemSocketingSocketButton)
+	ItemSocketingSocketButton:Point("BOTTOMRIGHT", -10, 39)
+
 	for i = 1, MAX_NUM_SOCKETS do
-		local button = _G[("ItemSocketingSocket%d"):format(i)]
-		local button_bracket = _G[("ItemSocketingSocket%dBracketFrame"):format(i)]
-		local button_bg = _G[("ItemSocketingSocket%dBackground"):format(i)]
-		local button_icon = _G[("ItemSocketingSocket%dIconTexture"):format(i)]
+		local button = _G["ItemSocketingSocket"..i]
+		local bracket = _G["ItemSocketingSocket"..i.."BracketFrame"]
+		local bg = _G["ItemSocketingSocket"..i.."Background"]
+		local icon = _G["ItemSocketingSocket"..i.."IconTexture"]
+		local shine = _G["ItemSocketingSocket"..i.."Shine"]
+
 		button:StripTextures()
 		button:StyleButton(false)
 		button:SetTemplate("Default", true)
-		button_bracket:Kill()
-		button_bg:Kill()
-		button_icon:SetTexCoord(unpack(E.TexCoords))
-		button_icon:SetInside()
+
+		bracket:Kill()
+		bg:Kill()
+
+		icon:SetTexCoord(unpack(E.TexCoords))
+		icon:SetInside()
+
+		shine:Point("CENTER")
+		shine:Size(40)
 	end
+
+	local GEM_TYPE_INFO = GEM_TYPE_INFO
 
 	hooksecurefunc("ItemSocketingFrame_Update", function()
 		local numSockets = GetNumSockets()
+
 		for i = 1, numSockets do
-			local button = _G[("ItemSocketingSocket%d"):format(i)]
-			local gemColor = GetSocketTypes(i)
-			local color = GEM_TYPE_INFO[gemColor]
+			local button = _G["ItemSocketingSocket"..i]
+			local color = GEM_TYPE_INFO[GetSocketTypes(i)]
 			button:SetBackdropColor(color.r, color.g, color.b, 0.15)
 			button:SetBackdropBorderColor(color.r, color.g, color.b)
 		end
+
+		if numSockets == 3 then
+			ItemSocketingSocket1:SetPoint("BOTTOM", -80, 70)
+		elseif numSockets == 2 then
+			ItemSocketingSocket1:SetPoint("BOTTOM", -36, 70)
+		else
+			ItemSocketingSocket1:SetPoint("BOTTOM", 0, 70)
+		end
 	end)
 
-	S:HandleButton(ItemSocketingSocketButton)
+	hooksecurefunc(ItemSocketingScrollFrame, "SetWidth", function(self, width)
+		if width == 269 then
+			self:Width(300)
+		elseif width == 297 then
+			self:Width(321)
+		end
+	end)
 end
 
 S:AddCallback("Sirus_ItemSocketing", LoadSkin)
