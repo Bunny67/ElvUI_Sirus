1eaaad5bf1cd1b8fcede0fc9a901f9d66b9fc4e3
Bunny67 <zolov6767@gmail.com>
2020-05-11 21:07:56 +0300

    experimental ArenaRating tooltip

--- ElvUI_Sirus/Modules/DataTexts/ArenaRating.lua
+++ ElvUI_Sirus/Modules/DataTexts/ArenaRating.lua
@@ -36,11 +36,41 @@ local function OnEvent(self)
 		end
 	end
 
-	self.text:SetText(tconcat(tbl, ", "))
+	self.text:SetText(tconcat(tbl, " "))
 end
 
 local function OnClick()
 	TogglePVPUIFrame()
+end
+
+local textFormat = "|cff00ff00%d|r |cffff0000%d|r %d%%"
+
+local function OnEnter(self)
+	DT:SetupTooltip(self)
+
+	local pvpStats = C_CacheInstance:Get("ASMSG_PVP_STATS", {})
+	for i, enabled in ipairs(E.db.datatexts.ArenaRating) do
+		if enabled then
+			DT.tooltip:AddLine(brackets[i])
+
+			if i == 4 then
+				local _, _, _, _, _, _, _, _, _, weekWins, weekGames, seasonWins, seasonGames = GetRatedBattlegroundRankInfo()
+				local weekPerc, seasonPerc = weekGames == 0 and 0 or math.ceil(weekWins / weekGames * 100), seasonGames == 0 and 0 or math.ceil(seasonWins / seasonGames * 100)
+
+				DT.tooltip:AddDoubleLine(PVP_LADDER_WEEK, format(textFormat, weekWins, weekGames - weekWins, weekPerc), 1, 1, 1, weekPerc >= 50 and 0 or 1, weekPerc >= 50 and 1 or 0, 0)
+				DT.tooltip:AddDoubleLine(PVP_LADDER_SEASON, format(textFormat, seasonWins, seasonGames - seasonWins, seasonPerc), 1, 1, 1, seasonPerc >= 50 and 0 or 1, seasonPerc >= 50 and 1 or 0, 0)
+			else
+				local todayWins, todayGames, weekWins, weekGames, seasonWins, seasonGames = pvpStats[i] and pvpStats[i].TodayWins or 0, pvpStats[i] and pvpStats[i].TodayGames or 0, pvpStats[i] and pvpStats[i].weekWins or 0, pvpStats[i] and pvpStats[i].weekGames or 0,pvpStats[i] and pvpStats[i].seasonWins or 0, pvpStats[i] and pvpStats[i].seasonGames or 0
+				local todayPerc, weekPerc, seasonPerc = todayGames == 0 and 0 or math.ceil(todayWins / todayGames * 100), weekGames == 0 and 0 or math.ceil(weekWins / weekGames * 100), seasonGames == 0 and 0 or math.ceil(seasonWins / seasonGames * 100)
+
+				DT.tooltip:AddDoubleLine(PVP_LADDER_DAY, format(textFormat, todayWins, todayGames - todayWins, todayPerc), 1, 1, 1, todayPerc >= 50 and 0 or 1, todayPerc >= 50 and 1 or 0, 0)
+				DT.tooltip:AddDoubleLine(PVP_LADDER_WEEK, format(textFormat, weekWins, weekGames - weekWins, weekPerc), 1, 1, 1, weekPerc >= 50 and 0 or 1, weekPerc >= 50 and 1 or 0, 0)
+				DT.tooltip:AddDoubleLine(PVP_LADDER_SEASON, format(textFormat, seasonWins, seasonGames - seasonWins, seasonPerc), 1, 1, 1, seasonPerc >= 50 and 0 or 1, seasonPerc >= 50 and 1 or 0, 0)
+			end
+		end
+	end
+
+	DT.tooltip:Show()
 end
 
 local function ValueColorUpdate(hex)
@@ -52,4 +82,4 @@ local function ValueColorUpdate(hex)
 end
 E.valueColorUpdateFuncs[ValueColorUpdate] = true
 
-DT:RegisterDatatext("ArenaRating", {"PLAYER_ENTERING_WORLD", "ZONE_CHANGED_NEW_AREA"}, OnEvent, nil, OnClick, nil, nil, PVP_YOUR_RATING)
+DT:RegisterDatatext("ArenaRating", {"PLAYER_ENTERING_WORLD", "ZONE_CHANGED_NEW_AREA"}, OnEvent, nil, OnClick, OnEnter, nil, PVP_YOUR_RATING)
