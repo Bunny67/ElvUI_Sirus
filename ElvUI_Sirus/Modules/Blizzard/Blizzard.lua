cb1105977708a56dd871d6a8d81a0ae8755c9e6d
Bunny67 <zolov6767@gmail.com>
2020-05-29 17:05:38 +0300

    test quest

---
+++ ElvUI_Sirus/Modules/Blizzard/Blizzard.lua
@@ -0,0 +1,16 @@
+local items = {
+	["QuestInfoItem"] = MAX_NUM_ITEMS,
+	["QuestProgressItem"] = MAX_REQUIRED_ITEMS
+}
+
+QuestFrame:HookScript("OnShow", function(self)
+	local frameLevel = self:GetFrameLevel()
+
+	if QuestInfoItem1:GetFrameLevel() <= frameLevel or QuestProgressItem1:GetFrameLevel() <= frameLevel then
+		for frame, numItems in pairs(items) do
+			for i = 1, numItems do
+				_G[frame..i]:SetFrameLevel(frameLevel:GetFrameLevel() + 5)
+			end
+		end
+	end
+end)
