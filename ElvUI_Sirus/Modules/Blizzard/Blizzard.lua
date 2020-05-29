db98027e8afd3e392bb44f5e9a7093c2bb4edbb1
Bunny67 <zolov6767@gmail.com>
2020-05-29 18:37:31 +0300

    oops

--- ElvUI_Sirus/Modules/Blizzard/Blizzard.lua
+++ ElvUI_Sirus/Modules/Blizzard/Blizzard.lua
@@ -9,7 +9,7 @@ QuestFrame:HookScript("OnShow", function(self)
 	if QuestInfoItem1:GetFrameLevel() <= frameLevel or QuestProgressItem1:GetFrameLevel() <= frameLevel then
 		for frame, numItems in pairs(items) do
 			for i = 1, numItems do
-				_G[frame..i]:SetFrameLevel(frameLevel:GetFrameLevel() + 5)
+				_G[frame..i]:SetFrameLevel(frameLevel + 5)
 			end
 		end
 	end
