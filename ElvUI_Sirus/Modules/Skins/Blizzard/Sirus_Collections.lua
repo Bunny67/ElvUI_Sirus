39bcf9a0581b3cf5cdc9e995ac8bba9ac51eea2d
fxpw <fxpw@outlook.com>
2021-11-11 21:34:12 +0300

    transmog add

--- ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_Collections.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_Collections.lua
@@ -7,161 +7,172 @@ local ipairs = ipairs
 
 local function LoadSkin()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.collections then return end
+		--tab for mount pet and transmog
+		for i = 1,3 do
+			S:HandleTab(_G["CollectionsJournalTab"..i])
+		end
+		S:HandlePortraitFrame(CollectionsJournal)
 
-	S:HandlePortraitFrame(CollectionsJournal)
+		MountJournal.navBar:StripTextures()
+		MountJournal.navBar.overlay:StripTextures()
 
-	MountJournal.navBar:StripTextures()
-	MountJournal.navBar.overlay:StripTextures()
+		S:HandleButton(MountJournal.navBar.home, true)
+		MountJournal.navBar.home.xoffset = 1
 
-	S:HandleButton(MountJournal.navBar.home, true)
-	MountJournal.navBar.home.xoffset = 1
+		MountJournal.navBar:ClearAllPoints()
+		MountJournal.navBar:SetPoint("TOPLEFT", 6, -22)
 
-	MountJournal.navBar:ClearAllPoints()
-	MountJournal.navBar:SetPoint("TOPLEFT", 6, -22)
+		S:HandleEditBox(MountJournal.searchBox)
 
-	S:HandleEditBox(MountJournal.searchBox)
+		MountJournal.FilterButton:StripTextures(true)
+		S:HandleButton(MountJournal.FilterButton)
 
-	MountJournal.FilterButton:StripTextures(true)
-	S:HandleButton(MountJournal.FilterButton)
+		MountJournal.ListScrollFrame:StripTextures()
+		S:HandleScrollBar(MountJournal.ListScrollFrame.scrollBar)
 
-	MountJournal.ListScrollFrame:StripTextures()
-	S:HandleScrollBar(MountJournal.ListScrollFrame.scrollBar)
-
-	for _, button in ipairs(MountJournal.ListScrollFrame.buttons) do
-		button.icon:Size(40)
-		button.icon:SetDrawLayer("BORDER")
-		S:HandleIcon(button.icon)
-
-		local highlight = button:GetHighlightTexture()
-		button:SetHighlightTexture(E.Media.Textures.Highlight)
-		highlight:SetTexCoord(0, 1, 0, 1)
-		highlight:SetVertexColor(1, 1, 1, .35)
-		highlight:SetAllPoints()
-
-		button.iconBorder:SetTexture()
-		button.background:SetTexture()
-
-		button.selectedTexture:SetTexture(E.Media.Textures.Highlight)
-		button.selectedTexture:SetTexCoord(0, 1, 0, 1)
-		button.selectedTexture:SetVertexColor(1, .8, .1, .35)
-		button.selectedTexture:SetAllPoints()
-
-		button.favorite:SetParent(button.backdrop)
-	end
-
-	for _, button in ipairs(MountJournal.CategoryScrollFrame.buttons) do
-		button:SetHighlightTexture(E.Media.Textures.Highlight)
-		button:GetHighlightTexture():SetVertexColor(1, 1, 1)
-		button:GetHighlightTexture().SetAlpha = E.noop
-
-		button.Background:SetTexture()
-
-		button.Icon:SetDrawLayer("BORDER")
-		S:HandleIcon(button.Icon)
-	end
-
-	hooksecurefunc("MountJournal_CategoryDisplayButton", function(button, element)
-		if element then
-			button.backdrop:SetShown(element.isCategory)
+		for _, button in ipairs(MountJournal.ListScrollFrame.buttons) do
+			button.icon:Size(40)
+			button.icon:SetDrawLayer("BORDER")
+			S:HandleIcon(button.icon)
 
 			local highlight = button:GetHighlightTexture()
+			button:SetHighlightTexture(E.Media.Textures.Highlight)
 			highlight:SetTexCoord(0, 1, 0, 1)
+			highlight:SetVertexColor(1, 1, 1, .35)
 			highlight:SetAllPoints()
+
+			button.iconBorder:SetTexture()
+			button.background:SetTexture()
+
+			button.selectedTexture:SetTexture(E.Media.Textures.Highlight)
+			button.selectedTexture:SetTexCoord(0, 1, 0, 1)
+			button.selectedTexture:SetVertexColor(1, .8, .1, .35)
+			button.selectedTexture:SetAllPoints()
+
+			button.favorite:SetParent(button.backdrop)
 		end
-	end)
 
-	MountJournal.CategoryScrollFrame:StripTextures()
-	S:HandleScrollBar(MountJournal.CategoryScrollFrame.scrollBar)
+		for _, button in ipairs(MountJournal.CategoryScrollFrame.buttons) do
+			button:SetHighlightTexture(E.Media.Textures.Highlight)
+			button:GetHighlightTexture():SetVertexColor(1, 1, 1)
+			button:GetHighlightTexture().SetAlpha = E.noop
 
-	MountJournal.LeftInset:StripTextures()
+			button.Background:SetTexture()
 
-	MountJournal.RightTopInset:StripTextures()
+			button.Icon:SetDrawLayer("BORDER")
+			S:HandleIcon(button.Icon)
+		end
 
-	MountJournal.RightBottomInset:StripTextures()
+		hooksecurefunc("MountJournal_CategoryDisplayButton", function(button, element)
+			if element then
+				button.backdrop:SetShown(element.isCategory)
 
-	MountJournal.MountCount:StripTextures()
+				local highlight = button:GetHighlightTexture()
+				highlight:SetTexCoord(0, 1, 0, 1)
+				highlight:SetAllPoints()
+			end
+		end)
 
-	MountJournal.MountDisplay:StripTextures()
+		MountJournal.CategoryScrollFrame:StripTextures()
+		S:HandleScrollBar(MountJournal.CategoryScrollFrame.scrollBar)
 
-	MountJournal.MountDisplay.ShadowOverlay:Hide()
+		MountJournal.LeftInset:StripTextures()
 
-	S:HandleRotateButton(MountDisplayModelSceneRotateLeftButton)
-	S:HandleRotateButton(MountDisplayModelSceneRotateRightButton)
+		MountJournal.RightTopInset:StripTextures()
 
-	-- Не нашел отдельного метода под ActionButton.
-	S:HandleItemButton(MountJournal.SummonRandomFavoriteButton)
+		MountJournal.RightBottomInset:StripTextures()
 
-	S:HandleIcon(MountJournal.MountDisplay.ModelScene.InfoButton.Icon)
-	S:HandleButton(MountJournal.MountDisplay.ModelScene.buyFrame.buyButton)
+		MountJournal.MountCount:StripTextures()
 
-	S:HandleButton(MountJournal.MountButton, true)
+		MountJournal.MountDisplay:StripTextures()
 
-	-- путеводитель
-	S:HandleButton(MountDisplayModelSceneEJFrameOpenEJButton)
-	S:HandleButton(PetJournalPetDisplayModelSceneEJFrameOpenEJButton)
+		MountJournal.MountDisplay.ShadowOverlay:Hide()
 
-	--tab for mount and pet
-	for i = 1,2 do
-	S:HandleTab(_G["CollectionsJournalTab"..i])
-	end
+		S:HandleRotateButton(MountDisplayModelSceneRotateLeftButton)
+		S:HandleRotateButton(MountDisplayModelSceneRotateRightButton)
 
-	-- pet tab
-	S:HandleRotateButton(PetJournalPetDisplayModelSceneRotateLeftButton)
-	S:HandleRotateButton(PetJournalPetDisplayModelSceneRotateRightButton)
-	S:HandleButton(PetJournalSummonButton, true)
+		-- Не нашел отдельного метода под ActionButton.
+		S:HandleItemButton(MountJournal.SummonRandomFavoriteButton)
 
-	S:HandleScrollBar(PetJournalListScrollFrameScrollBar)
+		S:HandleIcon(MountJournal.MountDisplay.ModelScene.InfoButton.Icon)
+		S:HandleButton(MountJournal.MountDisplay.ModelScene.buyFrame.buyButton)
 
-	PetJournalFilterButton:StripTextures(true)
-	S:HandleButton(PetJournalFilterButton)
+		S:HandleButton(MountJournal.MountButton, true)
 
-	S:HandleEditBox(PetJournalSearchBox)
-
-	--pet tab ListScrollFrame
-	for _, button in ipairs(PetJournal.ListScrollFrame.buttons) do
-
-		
-
-		local highlight = button:GetHighlightTexture()
-		button:SetHighlightTexture(E.Media.Textures.Highlight)
-		highlight:SetTexCoord(0, 1, 0, 1)
-		highlight:SetVertexColor(1, 1, 1, .35)
-		highlight:SetAllPoints()
-		print(highlight)
-		button.Background:SetTexture()
-
-		 button.Icon:SetDrawLayer("BORDER")
-		S:HandleIcon(button.Icon)
-		
-		 button.DragButton.Favorite:SetParent(button.backdrop)
+		-- путеводитель
+		S:HandleButton(MountDisplayModelSceneEJFrameOpenEJButton)
+		S:HandleButton(PetJournalPetDisplayModelSceneEJFrameOpenEJButton)
 
 		
-	end
 
-	-- right frame
+		-- pet tab
+		S:HandleRotateButton(PetJournalPetDisplayModelSceneRotateLeftButton)
+		S:HandleRotateButton(PetJournalPetDisplayModelSceneRotateRightButton)
+		S:HandleButton(PetJournalSummonButton, true)
 
-	PetJournal.LeftInset:StripTextures()
-	PetJournal.RightInset:StripTextures()
-	PetJournal.PetDisplay:StripTextures()
-	PetJournal.PetDisplay.ShadowOverlay:Hide()
+		S:HandleScrollBar(PetJournalListScrollFrameScrollBar)
 
-	--pet count
-	PetJournal.PetCount:StripTextures()
+		PetJournalFilterButton:StripTextures(true)
+		S:HandleButton(PetJournalFilterButton)
 
-	-- pet icon
-	S:HandleIcon(PetJournal.PetDisplay.InfoButton.Icon)
+		S:HandleEditBox(PetJournalSearchBox)
 
-	-- function fcngsirusjournal_onshow()
-	-- 	PetJournal:SetScript("OnUpdate",function()
-	-- 		if PetJournalSummonRandomFavoriteButton ~= nil then 
-	-- 			S:HandleItemButton(PetJournalSummonRandomFavoriteButton)
-	-- 		end
-	-- 						end)
-	-- end
-									
+		--pet tab ListScrollFrame
+		for _, button in ipairs(PetJournal.ListScrollFrame.buttons) do
 
-	-- PetJournal:HookScript("OnUpdate",fcngsirusjournal_onshow)
-	S:HandleItemButton(PetJournal.SummonRandomFavoritePetButton)
+			
+
+			local highlight = button:GetHighlightTexture()
+			button:SetHighlightTexture(E.Media.Textures.Highlight)
+			highlight:SetTexCoord(0, 1, 0, 1)
+			highlight:SetVertexColor(1, 1, 1, .35)
+			highlight:SetAllPoints()
+			print(highlight)
+			button.Background:SetTexture()
+
+			button.Icon:SetDrawLayer("BORDER")
+			S:HandleIcon(button.Icon)
+			
+			button.DragButton.Favorite:SetParent(button.backdrop)
+
+			
+		end
+
+		-- right frame
+
+		PetJournal.LeftInset:StripTextures()
+		PetJournal.RightInset:StripTextures()
+		PetJournal.PetDisplay:StripTextures()
+		PetJournal.PetDisplay.ShadowOverlay:Hide()
+
+		--pet count
+		PetJournal.PetCount:StripTextures()
+
+		-- pet icon
+		S:HandleIcon(PetJournal.PetDisplay.InfoButton.Icon)
+
+		-- function fcngsirusjournal_onshow()
+		-- 	PetJournal:SetScript("OnUpdate",function()
+		-- 		if PetJournalSummonRandomFavoriteButton ~= nil then 
+		-- 			S:HandleItemButton(PetJournalSummonRandomFavoriteButton)
+		-- 		end
+		-- 						end)
+		-- end
+										
+
+		-- PetJournal:HookScript("OnUpdate",fcngsirusjournal_onshow)
+		-- S:SkinProfessionButton(PetJournal.SummonRandomFavoritePetButton)
+			S:HandleItemButton(_G.PetJournalSummonRandomFavoritePetButton, true)
+			E:RegisterCooldown(_G.PetJournalSummonRandomFavoritePetButtonCooldown)
+			_G.PetJournalSummonRandomFavoritePetButtonCooldown:SetAllPoints(_G.PetJournalSummonRandomFavoritePetButtonIconTexture)
+		-- S:HandleIcon(PetJournal.SummonRandomFavoritePetButton.Icon)
+		-- PetJournal.SummonRandomFavoritePetButton.Border:StripTextures()
+		-- S:HandleIcon(PetJournal.SummonRandomFavoritePetButton.IconTexture)
+		-- PetJournal.SummonRandomFavoritePetButton:StripTextures()
+		-- PetJournal.SummonRandomFavoritePetButton:CreateBackdrop("Default", true)
+		-- S:HandleIcon(PetJournal.SummonRandomFavoritePetButton)
+
+		
 end
 
 S:AddCallback("Skin_Collections", LoadSkin)
--- ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_Transmogrify.lua
+++ ElvUI_Sirus/Modules/Skins/Blizzard/Sirus_Transmogrify.lua
@@ -9,45 +9,254 @@ local unpack = unpack
 local function LoadSkin()
 	if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard.transmogrify ~= true then return end
 
-	S:HandlePortraitFrame(TransmogrifyFrame)
-	S:HandleCloseButton(TransmogrifyFrameCloseButton)
-	TransmogrifyFrameArt:StripTextures()
+-- 	S:HandlePortraitFrame(TransmogrifyFrame)
+-- 	S:HandleCloseButton(TransmogrifyFrameCloseButton)
+-- 	TransmogrifyFrameArt:StripTextures()
 
-	S:HandleRotateButton(TransmogrifyModelFrameRotateLeftButton)
-	S:HandleRotateButton(TransmogrifyModelFrameRotateRightButton)
-	TransmogrifyModelFrameRotateRightButton:Point("TOPLEFT", TransmogrifyModelFrameRotateLeftButton, "TOPRIGHT", 5, 0)
+-- 	S:HandleRotateButton(TransmogrifyModelFrameRotateLeftButton)
+-- 	S:HandleRotateButton(TransmogrifyModelFrameRotateRightButton)
+-- 	TransmogrifyModelFrameRotateRightButton:Point("TOPLEFT", TransmogrifyModelFrameRotateLeftButton, "TOPRIGHT", 5, 0)
 
-	local slots = {
-		"HeadSlot",
-		"ShoulderSlot",
-		"BackSlot",
-		"ChestSlot",
-		"WristSlot",
-		"HandsSlot",
-		"WaistSlot",
-		"LegsSlot",
-		"FeetSlot",
-		"MainHandSlot",
-		"SecondaryHandSlot",
-		"RangedSlot"
-	}
+-- 	local slots = {
+-- 		"HeadSlot",
+-- 		"ShoulderSlot",
+-- 		"BackSlot",
+-- 		"ChestSlot",
+-- 		"WristSlot",
+-- 		"HandsSlot",
+-- 		"WaistSlot",
+-- 		"LegsSlot",
+-- 		"FeetSlot",
+-- 		"MainHandSlot",
+-- 		"SecondaryHandSlot",
+-- 		"RangedSlot"
+-- 	}
 
-	for i, slot in pairs(slots) do
-		local button = _G["TransmogrifyFrame"..slot]
-		button:SetTemplate()
-		button:StyleButton()
+-- 	for i, slot in pairs(slots) do
+-- 		local button = _G["TransmogrifyFrame"..slot]
+-- 		button:SetTemplate()
+-- 		button:StyleButton()
 
-		button.icon:SetTexCoord(unpack(E.TexCoords))
-		button.icon:SetInside()
-		button.icon:SetDrawLayer("BORDER")
-		button.altTexture:SetAlpha(0)
+-- 		button.icon:SetTexCoord(unpack(E.TexCoords))
+-- 		button.icon:SetInside()
+-- 		button.icon:SetDrawLayer("BORDER")
+-- 		button.altTexture:SetAlpha(0)
 
-		_G["TransmogrifyFrame"..slot.."Border"]:SetAlpha(0)
-		_G["TransmogrifyFrame"..slot.."Grabber"]:SetAlpha(0)
-	end
+-- 		_G["TransmogrifyFrame"..slot.."Border"]:SetAlpha(0)
+-- 		_G["TransmogrifyFrame"..slot.."Grabber"]:SetAlpha(0)
+-- 	end
 
-	S:HandleButton(TransmogrifyApplyButton, true)
-	TransmogrifyFrameButtonFrame:StripTextures()
+-- 	S:HandleButton(TransmogrifyApplyButton, true)
+-- 	TransmogrifyFrameButtonFrame:StripTextures()
+
+
+
+		-- transmog tab
+
+		WardrobeFrame:StripTextures()
+		WardrobeFrame:CreateBackdrop("Transperent")
+		S:HandleCloseButton(WardrobeFrameCloseButton)
+
+		WardrobeTransmogFrame:StripTextures()
+		-- WardrobeTransmogFrame:CreateBackdrop("Transperent")
+
+		WardrobeTransmogFrameModelFrame:StripTextures()
+		-- WardrobeTransmogFrameModelFrame:CreateBackdrop("Transperent")
+		local function refreshbutton_onshow()
+			WardrobeCollectionFrameFilterButton:StripTextures()
+			WardrobeCollectionFrameFilterButton:CreateBackdrop()
+			S:HandleButton(WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton)
+			-- WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton:StripTextures()
+			-- WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton:CreateBackdrop()
+			-- local iconrefresh = AuctionHouseFrameCommoditiesBuyFrameItemListRefreshFrameRefreshButtonIcon:GetTexture();
+			-- WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.Icon:SetTexture(iconrefresh)
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.Icon:SetDrawLayer("ARTWORK")
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.TopLeft:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.TopRight:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.BottomLeft:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.BottomRight:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.TopMiddle:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.MiddleLeft:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.MiddleRight:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.BottomMiddle:Hide()
+			WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.MiddleMiddle:Hide()
+			-- WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton.BottomLeft:Hide()
+		end
+			
+
+		WardrobeTransmogFrame.ModelFrame.ClearAllPendingButton:SetScript("OnUpdate",refreshbutton_onshow)
+
+		S:HandleNextPrevButton(WardrobeCollectionFrameItemsCollectionFramePagingFramePrevPageButton, nil, nil, true)
+		S:HandleNextPrevButton(WardrobeCollectionFrameItemsCollectionFramePagingFrameNextPageButton, nil, nil, true)
+
+		S:HandleDropDownBox(WardrobeCollectionFrameItemsCollectionFrameWeaponDropDown)
+		S:HandleTab(WardrobeCollectionFrameTab1)
+		WardrobeCollectionFrameTab1:StripTextures()
+		S:HandleEditBox(WardrobeCollectionFrameSearchBox)
+		
+
+		WardrobeCollectionFrameItemsCollectionFrameOverlayFrame:StripTextures()
+		WardrobeCollectionFrameItemsCollectionFrameOverlayFrame:CreateBackdrop("Transperent")
+		WardrobeCollectionFrameItemsCollectionFrame:StripTextures()
+
+
+		local sbcR, sbcG, sbcB = 4/255, 179/255, 30/255
+
+		local function skinStatusBar(bar)
+			bar:StripTextures()
+			bar:SetStatusBarTexture(E.media.normTex)
+			bar:SetStatusBarColor(sbcR, sbcG, sbcB)
+			bar:CreateBackdrop("Default")
+			E:RegisterStatusBar(bar)
+	
+			local barName = bar:GetName()
+			local title = _G[barName.."Title"]
+			local label = _G[barName.."Label"]
+			local text = _G[barName.."Text"]
+	
+			if title then
+				title:Point("LEFT", 4, 0)
+			end
+	
+			if label then
+				label:Point("LEFT", 4, 0)
+			end
+	
+			if text then
+				text:Point("RIGHT", -4, 0)
+			end
+		end
+
+
+		skinStatusBar(WardrobeCollectionFrameProgressBar)
+
+		--buttons top 
+		-- S:HandleButton(WardrobeCollectionFrameItemsCollectionFrameSlotsFrameSlotButton1)
+		local function stripbutton_onup()
+			S:HandleButton(WardrobeCollectionFrameFilterButton)
+			-- WardrobeCollectionFrameFilterButton:CreateBackdrop()
+			WardrobeCollectionFrame.FilterButton.TopLeft:Hide()
+			WardrobeCollectionFrame.FilterButton.TopRight:Hide()
+			WardrobeCollectionFrame.FilterButton.BottomLeft:Hide()
+			WardrobeCollectionFrame.FilterButton.BottomRight:Hide()
+			WardrobeCollectionFrame.FilterButton.TopMiddle:Hide()
+			WardrobeCollectionFrame.FilterButton.MiddleLeft:Hide()
+			WardrobeCollectionFrame.FilterButton.MiddleRight:Hide()
+			WardrobeCollectionFrame.FilterButton.BottomMiddle:Hide()
+			WardrobeCollectionFrame.FilterButton.MiddleMiddle:Hide()
+			
+					end
+		WardrobeCollectionFrameFilterButton:SetScript("OnUpdate",stripbutton_onup)
+						
+		
+		local function clearBackdrop(self)
+			self:SetBackdropColor(0, 0, 0, 0)
+		end
+
+		for _, Frame in ipairs(WardrobeCollectionFrame.ContentFrames) do
+			if Frame.Models then
+				for _, Model in pairs(Frame.Models) do
+					Model.Overlay.Border:SetAlpha(0)
+					Model.Overlay.TransmogStateTexture:SetAlpha(0)
+	
+					local border = CreateFrame('Frame', nil, Model)
+					border:SetTemplate()
+					border:ClearAllPoints()
+					border:SetPoint('TOPLEFT', Model, 'TOPLEFT', 0, 1) -- dont use set inside, left side needs to be 0
+					border:SetPoint('BOTTOMRIGHT', Model, 'BOTTOMRIGHT', 1, -1)
+					border:SetBackdropColor(0, 0, 0, 0)
+					border.callbackBackdropColor = clearBackdrop
+	
+					if Model.NewGlow then Model.NewGlow:SetParent(border) end
+					if Model.NewString then Model.NewString:SetParent(border) end
+	
+					for i=1, Model:GetNumRegions() do
+						local region = select(i, Model:GetRegions())
+						if region:IsObjectType('Texture') then -- check for hover glow
+							local texture = region:GetTexture()
+							if texture == 1116940 or texture == 1569530 then -- transmogrify.blp (items:1116940 or sets:1569530)
+								region:SetColorTexture(1, 1, 1, 0.3)
+								region:SetBlendMode('ADD')
+								region:SetAllPoints(Model)
+							end
+						end
+					end
+	
+					hooksecurefunc(Model.Overlay.Border, 'SetAtlas', function(_, texture)
+						if texture == 'transmog-wardrobe-border-uncollected' then
+							border:SetBackdropBorderColor(0.9, 0.9, 0.3)
+						elseif texture == 'transmog-wardrobe-border-unusable' then
+							border:SetBackdropBorderColor(0.9, 0.3, 0.3)
+						elseif Model.Overlay.TransmogStateTexture:IsShown() then
+							border:SetBackdropBorderColor(1, 0.7, 1)
+						else
+							border:SetBackdropBorderColor(unpack(E.media.bordercolor))
+						end
+					end)
+				end
+			end
+	
+			-- local pending = Frame.PendingTransmogFrame
+			-- if pending then
+			-- 	local Glowframe = pending.Glowframe
+			-- 	-- Glowframe:SetAtlas(nil)
+			-- 	-- Glowframe:CreateBackdrop(nil, nil, nil, nil, nil, nil, nil, nil, pending:GetFrameLevel())
+	
+			-- 	if Glowframe.backdrop then
+			-- 		Glowframe.backdrop:SetPoint('TOPLEFT', pending, 'TOPLEFT', 0, 1) -- dont use set inside, left side needs to be 0
+			-- 		Glowframe.backdrop:SetPoint('BOTTOMRIGHT', pending, 'BOTTOMRIGHT', 1, -1)
+			-- 		Glowframe.backdrop:SetBackdropBorderColor(1, 0.7, 1)
+			-- 		Glowframe.backdrop:SetBackdropColor(0, 0, 0, 0)
+			-- 	end
+	
+			-- 	for i = 1, 12 do
+			-- 		if i < 5 then
+			-- 			Frame.PendingTransmogFrame['Smoke'..i]:Hide()
+			-- 		end
+	
+			-- 		Frame.PendingTransmogFrame['Wisp'..i]:Hide()
+			-- 	end
+			-- end
+	
+			-- local paging = Frame.PagingFrame
+			-- if paging then
+			-- 	S:HandleNextPrevButton(paging.PrevPageButton, nil, nil, true)
+			-- 	S:HandleNextPrevButton(paging.NextPageButton, nil, nil, true)
+			-- end
+
+			S:HandleButton(WardrobeTransmogFrameApplyButton)
+			
+			for i = 1, #WardrobeTransmogFrame.SlotButtons do
+				local slotButton = WardrobeTransmogFrame.SlotButtons[i]
+				slotButton:SetFrameLevel(slotButton:GetFrameLevel() + 2)
+				slotButton:StripTextures()
+				slotButton:CreateBackdrop(nil, nil, nil, nil, nil, nil, nil, true)
+				slotButton.Border:Kill()
+				slotButton.Icon:SetTexCoord(unpack(E.TexCoords))
+				slotButton.Icon:SetInside(slotButton.backdrop)
+
+				local undo = slotButton.UndoButton
+				if undo then undo:SetHighlightTexture(nil) end
+
+				local pending = slotButton.PendingFrame
+				if pending then
+					if slotButton.transmogType == 1 then
+						pending.Glow:Size(48)
+						pending.Ants:Size(30)
+					else
+						pending.Glow:Size(74)
+						pending.Ants:Size(48)
+					end
+				end
+			end
+		end
+
+
+
 end
+
+
+
+
 
 S:AddCallback("Sirus_Transmogrify", LoadSkin)
